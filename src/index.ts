15866e2201aa744dc664cba6686d4a519c38c1a7
bekan849 <bekanbk1@gmail.com>
2025-12-01 23:49:24 -0400

    Microservicio de productos completo

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,5 @@ import app from "./app";
 const PORT = process.env.PORT || 4002;
 
 app.listen(PORT, () => {
-  console.log(`✅ Microservicio de productos corriendo en http://localhost:${PORT}`);
+  console.log(`✅ Microservicio de productos corriendo en ${PORT}`);
 });
